cace095da215aa2d58027a06fc792b64c56baaec
henrybee89 <henry89bee@gmail.com>
2017-02-27 08:51:06 -0800

    Calendar Project 1st commit

---
+++ Days.cpp
@@ -0,0 +1,10 @@
+#include "Days.h"
+#include <string>
+using namespace std;
+
+
+void Days::printWeek (string x)
+{
+	cout << x << endl;
+}
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Days.h"
+using namespace std;
+
+
+int main()
+{
+	Days Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+	Monday.day = "Monday";
+	Tuesday.day = "Tuesday";
+	cout << Monday.day << Tuesday.day;
+
+
+	return 0;
+}
+
