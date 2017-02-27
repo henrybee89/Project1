1d3418decd18e81bfe084216ed651ae75fa2595c
henrybee89 <henry89bee@gmail.com>
2017-02-27 10:19:47 -0800

    Added class Months

---
+++ Months.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class Months
+{
+	public:
+		std::string month;
+		int daysInMonth;
+
+//trevis42@gmail.com
+};
--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
 #include "Days.h"
+#include "Months.h"
 using namespace std;
 
 
 int main()
 {
 	Days Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+	Months January, February, March, April, May, June, July, August, September, October, November, December;
+
+	January.daysInMonth = 31;
+
 	Monday.day = "Monday";
 	Tuesday.day = "Tuesday";
-	cout << Monday.day << Tuesday.day;
+	Wednesday.day = "Wednesday";
+	Thursday.day = "Thursday";
+	Friday.day = "Friday";
+	Saturday.day = "Saturday";
+	Sunday.day = "Sunday";
+
+	cout << Monday.day << Tuesday.day << Wednesday.day << Thursday.day << Friday.day << Saturday.day << Sunday.day;
 
 
 	return 0;
